f493a29041dd10b11005a031267f0411dc873e96
codeaken <magnus@codeaken.com>
2014-10-15 22:04:31 +0200

    Added my info in workbench

--- app/config/workbench.php
+++ app/config/workbench.php
@@ -13,7 +13,7 @@ return array(
     |
     */
 
-    'name' => '',
+    'name' => 'Magnus Johansson',
 
     /*
     |--------------------------------------------------------------------------
@@ -26,6 +26,6 @@ return array(
     |
     */
 
-    'email' => '',
+    'email' => 'magnus@codeaken.com',
 
 );
